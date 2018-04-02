6430c0cb422d3256258e235f814961a74c6b6a88
abhijitshete <abhi.shete4@gmail.com>
2018-04-02 15:07:54 +0530

    slide nav problem solved

--- src/app/main-people/main-people.component.ts
+++ src/app/main-people/main-people.component.ts
@@ -1,46 +1,59 @@
-import { Component, OnInit,trigger, state, style, transition, animate, Input, Output,EventEmitter } from '@angular/core';
-import {NgbModal, ModalDismissReasons, NgbModalRef, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, trigger, state, style, transition, animate, Input, Output, EventEmitter } from '@angular/core';
+import { NgbModal, ModalDismissReasons, NgbModalRef, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ShareDataService } from "../share-data.service";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {AsideComponent} from "../aside/aside.component"
+import { AsideComponent } from "../aside/aside.component"
 @Component({
   selector: 'app-main-people',
   templateUrl: './main-people.component.html',
-  styleUrls: ['./main-people.component.scss']
+  styleUrls: ['./main-people.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ],
 })
 export class MainPeopleComponent implements OnInit {
 
 
   ngOnInit() {
   }
-  industryArr:Array<any>=[
-    {name:"Отрасль",name1:"Сегмент",extra:" Название компании",description:"Краткое описание чего-то там с ограничением",image:'../../assets/images/ava3.jpg'},
-    {name:"Отрасль",name1:"Сегмент",extra:" Название компании",description:"Краткое описание чего-то там с ограничением",image:'../../assets/images/ava3.jpg'},
-    {name:"Отрасль",name1:"Сегмент",extra:" Название компании",description:"Краткое описание чего-то там с ограничением",image:'../../assets/images/ava3.jpg'},
-    {name:"Отрасль",name1:"Сегмент",extra:" Название компании",description:"Краткое описание чего-то там с ограничением",image:'../../assets/images/ava3.jpg'},
-    {name:"Отрасль",name1:"Сегмент",extra:" Название компании",description:"Краткое описание чего-то там с ограничением",image:'../../assets/images/ava3.jpg'},
-    {name:"Отрасль",name1:"Сегмент",extra:" Название компании",description:"Краткое описание чего-то там с ограничением",image:'../../assets/images/ava3.jpg'},
+  industryArr: Array<any> = [
+    { name: "Отрасль", name1: "Сегмент", extra: " Название компании", description: "Краткое описание чего-то там с ограничением", image: '../../assets/images/ava3.jpg' },
+    { name: "Отрасль", name1: "Сегмент", extra: " Название компании", description: "Краткое описание чего-то там с ограничением", image: '../../assets/images/ava3.jpg' },
+    { name: "Отрасль", name1: "Сегмент", extra: " Название компании", description: "Краткое описание чего-то там с ограничением", image: '../../assets/images/ava3.jpg' },
+    { name: "Отрасль", name1: "Сегмент", extra: " Название компании", description: "Краткое описание чего-то там с ограничением", image: '../../assets/images/ava3.jpg' },
+    { name: "Отрасль", name1: "Сегмент", extra: " Название компании", description: "Краткое описание чего-то там с ограничением", image: '../../assets/images/ava3.jpg' },
+    { name: "Отрасль", name1: "Сегмент", extra: " Название компании", description: "Краткое описание чего-то там с ограничением", image: '../../assets/images/ava3.jpg' },
   ];
 
   modalRef: NgbModalRef;
   closeResult: string;
   message: string;
   sideBarIsOpened = false;
-  menuState2:string='in';
-  constructor(private modalService: NgbModal,private data: ShareDataService,public AsideComponent:AsideComponent,public BrowserAnimationsModule:BrowserAnimationsModule) { }
-  closeModal(): void{
-       let localModalRef = this.modalRef;
-       localModalRef.close();
-   }
-   newMessage() {
-     //his.toggleMenu();
-      this.data.changeMessage("Hello from Sibling")
-    }
+  menuState2: string = 'out';
+  constructor(private modalService: NgbModal, private data: ShareDataService, public AsideComponent: AsideComponent, public BrowserAnimationsModule: BrowserAnimationsModule) { }
+  closeModal(): void {
+    let localModalRef = this.modalRef;
+    localModalRef.close();
+  }
+  newMessage() {
+    //his.toggleMenu();
+    this.data.changeMessage("Hello from Sibling")
+  }
   open(modalPro) {
+    this.toggleMenu();
     this.AsideComponent.toggleMenu();
     this.newMessage();
     this.data.currentMessage.subscribe(message => this.message = "hello")
-    this.sideBarIsOpened=!this.sideBarIsOpened;
+    this.sideBarIsOpened = !this.sideBarIsOpened;
     // this.closeModal();
     this.modalRef = this.modalService.open(modalPro);
     this.modalRef.result.then((result) => {
@@ -56,6 +69,7 @@ export class MainPeopleComponent implements OnInit {
     // const modalRef = this.modalService.open(modalPro);
   }
   openModal1(modalPro) {
+    this.toggleMenu();
     console.log("inside openmodal 1");
     this.closeModal();
     this.modalRef = this.modalService.open(modalPro);
@@ -65,15 +79,15 @@ export class MainPeopleComponent implements OnInit {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
-    @Output() userEv = new EventEmitter<string>();
-  sidebar(){
-    if(this.menuState2=='out'){
-      this.menuState2="in";
+  @Output() userEv = new EventEmitter<string>();
+  sidebar() {
+    if (this.menuState2 == 'out') {
+      this.menuState2 = "in";
     }
-    else{
-        this.menuState2="out";
+    else {
+      this.menuState2 = "out";
     }
-    console.log("inside side bar",this.menuState2);
+    console.log("inside side bar", this.menuState2);
     this.userEv.emit(this.menuState2)
   }
 
@@ -85,5 +99,17 @@ export class MainPeopleComponent implements OnInit {
     } else {
       return `with: ${reason}`;
     }
+  }
+  receiveMessage($event) {
+    console.log("$event==>", $event)
+    if ($event != '') {
+      this.menuState2 = $event
+      this.toggleMenu();
+    }
+    // console.log(this.message);
+  }
+  toggleMenu() {
+    // 1-line if statement that toggles the value:
+    this.menuState2 = this.menuState2 === 'out' ? 'in' : 'out';
   }
 }
